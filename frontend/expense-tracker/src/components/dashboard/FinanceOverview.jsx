2ebea00fd69f6987620684e2b9b78605f9ace39a
Abdullah Ansari <abdullah4nsari@gmail.com>
2025-07-17 01:43:47 +0530

    API's Complete, Making frontend and integrating backend with frontend

---
+++ frontend/expense-tracker/src/components/dashboard/FinanceOverview.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import CustomPieChart from '../charts/CustomPieChart'
+
+const COLORS = ["#875CF5","#00c951","#FA2C37"]
+const FinanceOverview = ({totalBalance, totalIncome, totalExpense}) => {
+    const balanceData = [
+        {name: "Total Balance", amount: totalBalance},
+        {name: "Total Income", amount: totalIncome},
+        {name: "Total Expense", amount: totalExpense},
+    ]
+  return (
+    <div className='card'>
+      <div className='flex items-center justify-between'>
+        <h5 className='text-lg'>Financial Overview</h5>
+      </div>
+
+      <CustomPieChart
+      data={balanceData}
+      label="Total Balance"
+      totalAmount={`${totalBalance}`}
+      colors={COLORS}
+      showTextAnchor
+      />
+    </div>
+  )
+}
+
+export default FinanceOverview
--- frontend/expense-tracker/src/context/UserContext.jsx
+++ frontend/expense-tracker/src/context/UserContext.jsx
@@ -4,16 +4,22 @@ import { createContext } from "react";
 export const UserContext = createContext();
 
 export const UserProvider = ({children}) => {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
 
     //function to update user data
     const updateUser = (userData) => {
         setUser(userData);
+        localStorage.setItem("user", JSON.stringify(userData));
     };
 
     //function to clear user data (on logout)
     const clearUser = ()=>{
         setUser(null);
+        localStorage.removeItem("user");
+    localStorage.removeItem("token"); // optional
     };
 
     return (
